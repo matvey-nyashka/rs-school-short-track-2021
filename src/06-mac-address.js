ee99bf8fb4c822c0d9741f9032de23f2f2a279e9
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-04-08 16:06:49 +0300

    feat: added Mac function

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,12 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  if (n.length > 17) return false;
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] > 9 || n[i] > 'F') return false;
+  }
+  return true;
 }
 
 module.exports = isMAC48Address;
