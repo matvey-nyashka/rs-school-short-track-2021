6d2a2fa1bbe411e554f5ed8f3887ddf782255dcf
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-04-12 20:18:49 +0300

    feat: added getDNSStats function

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,23 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const obj = {};
+  let test;
+  let arr = domains.sort((a, b) => a.length - b.length);
+  arr = arr.map((value) => value.split('.'));
+  for (let i = 0; i < arr.length; i++) {
+    test = '';
+    for (let j = arr[i].length - 1; j > -1; j--) {
+      test += `.${arr[i][j]}`;
+      if (obj[test]) {
+        obj[test]++;
+      } else {
+        obj[test] = 1;
+      }
+    }
+  }
+  return obj;
 }
 
 module.exports = getDNSStats;
