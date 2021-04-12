40491a5176feb9129d9bc14b40cf9625f0e59964
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-04-12 23:02:09 +0300

    feat: added encodeLine function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let test = 1;
+  let string = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      test++;
+    } else if (test > 1) {
+      string += `${test}${str[i]}`;
+      test = 1;
+    } else {
+      string += `${str[i]}`;
+      test = 1;
+    }
+  }
+  return string;
 }
 
 module.exports = encodeLine;
